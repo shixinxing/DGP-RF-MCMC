7a2ac369087d3f555a7b15cad4732380ea671251
shixinxing <47957281+shixinxing@users.noreply.github.com>
2022-06-15 01:16:35 +0800

    add preconditioner implementation

--- dgp.py
+++ dgp.py
@@ -109,46 +109,87 @@ class DGP_RF(tf.Module):
         log_likelihood = tf.reduce_sum(self.log_likelihood(X_batch, Y_batch)) / batch_size
         return - (log_prior_sum + log_likelihood)
 
-
-    def sghmc_update(self, X_batch, Y_batch, lr, data_size, eta=0.9, temperature=1.):
+    def sgmcmc_update(self, X_batch, Y_batch, data_size, lr=0.01, beta=0.95, temperature=1.):
         """
-        Standard SGLD and SGHMC:
-        :param eta: eta = 1. SGLD; eta < 1, SGHMC
+        :param lr: here the learning rate is the instantaneous effect of the average-minibatch-gradient
+        on the parameter. Must be positive and typically is in a range from 1.0e-6 to 0.1.
+        :param beta: momentum_decay, in the range [0,1),
+        typically close to 1, e.g. values such as 0.75, 0.8, 0.9, 0.95.
         """
         with tf.GradientTape() as tape:
             U = self.U(X_batch, Y_batch, data_size)
         grads = tape.gradient(U, self.trainable_variables)
 
+        h = tf.sqrt(lr / data_size)
         for param, grad in zip(self.trainable_variables, grads):
-            if not hasattr(param, "auxiliary_v"):
-                # param.auxiliary_v = tf.zeros_like(param)
-                param.auxiliary_v = tf.random.normal(tf.shape(param))
-            v_new = (1. - eta) * param.auxiliary_v - lr * grad
+            assert hasattr(param, "auxiliary_m"), "Trainable Params do not have attr auxiliary_m!"
+            m_new = beta * param.auxiliary_m - h * data_size * grad
             eps = tf.random.normal(tf.shape(param))
-            v_new += (2.0 * lr * eta * temperature / data_size) ** 0.5 * eps
-            param.auxiliary_v = v_new
-            param.assign_add(v_new)
+            m_new = m_new + (2.0 * (1. - beta) * temperature) ** 0.5 * tf.sqrt(param.precond_M) * eps
+            param.auxiliary_m = m_new
+            param.assign_add(h * tf.math.reciprocal(param.precond_M) * m_new)
 
-    def csgmcmc_update(self, X_batch, Y_batch, lr, data_size, eta=0.9,
-                     epoch=None, epoch_per_cycle=50, num_samples_per_cycle=5, temperature=1.):
-        with tf.GradientTape() as tape:
-            U = self.U(X_batch, Y_batch, data_size)
-        grads = tape.gradient(U, self.trainable_variables)
+    def precond_update(self, ds, data_size, K_batches=32, precond_type='adagrad', rho_rms=0.99):
+        """
+        :param ds: tf.data.Dataset as Iterable
+        :param data_size: N
+        :param K_batches: K number of mini-batches
+        :return: preconditioner M w.r.t each param
+        """
+        if precond_type == 'identity':
+            for param in self.trainable_variables:
+                if not hasattr(param, "precond_M"):
+                    param.precond_M = tf.constant(1., dtype=tf.float32)
+                if not  hasattr(param, "auxiliary_m"):
+                    # param.auxiliary_m = tf.zeros_like(param)
+                    param.auxiliary_m = tf.random.normal(tf.shape(param))
+        else:
+            for param in self.trainable_variables:
+                if not hasattr(param, "m_c"):
+                    param.m_c = None
+                if not hasattr(param, "precond_M"):
+                    param.precond_M = tf.ones_like(param, dtype=tf.float32)
+                if not hasattr(param, "auxiliary_m"):
+                    # param.auxiliary_m = tf.zeros_like(param)
+                    param.auxiliary_m = tf.random.normal(tf.shape(param))
+                param.m_c = tf.math.rsqrt(param.precond_M) * param.auxiliary_m
 
-        for param, grad in zip(self.trainable_variables, grads):
-            if not hasattr(param, "auxiliary_v"):
-                param.auxiliary_v = tf.zeros_like(param)
-                # param.auxiliary_v = tf.random.normal(tf.shape(param))
-            v_new = (1. - eta) * param.auxiliary_v - lr * grad
-            if (epoch is None) or (epoch % epoch_per_cycle) + 1 > epoch_per_cycle - num_samples_per_cycle:
-                eps = tf.random.normal(tf.shape(param))
-                v_new += (2.0 * lr * eta * temperature / data_size) ** 0.5 * eps
-            param.auxiliary_v = v_new
-            param.assign_add(v_new)
+            DEFAULT_REGULARIZATION = 1.0e-7
+            k = 0
+            for X_batch, Y_batch in ds:
+                with tf.GradientTape() as tape:
+                    U = self.U(X_batch, Y_batch, data_size)
+                grads = tape.gradient(U, self.trainable_variables)
 
-    # def sgmcmc_update(self, X_batch, Y_batch, h, data_size, gamma, temperature=1.):
-    #     with tf.GradientTape() as tape:
-    #         U = self.U(X_batch, Y_batch, data_size)
-    #     grads = tape.gradient(U, self.trainable_variables)
-    #
-    #     for param, gard in zip(self.trainable_variables, grads):
+                for param, grad in zip(self.trainable_variables, grads):
+                    if not hasattr(param, 'v_sum'):
+                        param.v_sum = tf.zeros_like(param, dtype=tf.float32)
+                    if precond_type == 'adagrad':
+                        param.v_sum = param.v_sum + tf.square(grad)
+                    elif precond_type == 'rmsprop':
+                        param.v_sum = rho_rms * param.v_sum + (1.- rho_rms) * tf.square(grad)
+                k = k + 1
+                if k >= K_batches:
+                    break
+
+            sigma_min = None
+            for param in self.trainable_variables:
+                sigma = param.v_sum / K_batches + DEFAULT_REGULARIZATION
+                sigma = tf.sqrt(sigma)
+                tmp = tf.reduce_min(sigma)
+                if sigma_min is None:
+                    sigma_min = tmp
+                elif tmp < sigma_min:
+                    sigma_min = tmp
+                if not hasattr(param, "sigma"):
+                    param.sigma = None
+                param.sigma = sigma
+
+            for param in self.trainable_variables:
+                param.precond_M = param.sigma / sigma_min
+                param.auxiliary_m = tf.math.sqrt(param.precond_M) * param.m_c
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -19,3 +19,16 @@ class BNN_from_list(tf.Module):
 
 def log_gaussian(x, mean=0., var=1.):
     return - 0.5 * (tf.math.log(2. * np.pi) + tf.math.log(var) + tf.square(x - mean) / var)
+
+
+def cyclical_lr_schedule(lr_0, k, cycle_length):
+    """
+    adjust learning rate according to cyclical schedule
+    :param lr_0: initial learning rate;
+    :param k: current epoch, from 0;
+    :param cycle_length: Number of epoches in each cycle;
+    """
+    cos_inner = k % cycle_length
+    cos_inner = tf.constant(np.pi) / cycle_length * cos_inner
+    lr = lr_0 / 2. * (tf.math.cos(cos_inner) + 1.)
+    return lr
