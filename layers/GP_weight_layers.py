dcbed29f0288383ad5238e4e7fabc8e135d17e75
shixinxing <47957281+shixinxing@users.noreply.github.com>
2022-06-16 23:22:37 +0800

    fix layer name

--- layers/GP_weight_layers.py
+++ layers/GP_weight_layers.py
@@ -6,7 +6,7 @@ class GPLayer(tf.Module):
         super(GPLayer, self).__init__(name=name)
         self.in_feature = in_feature
         self.out_feature = out_feature
-        self.W = tf.Variable(tf.random.normal([in_feature, out_feature]), name='GP layer W')
+        self.W = tf.Variable(tf.random.normal([in_feature, out_feature]), name='GP_layer_W')
 
     def __call__(self, X):
         return tf.matmul(X, self.W)
