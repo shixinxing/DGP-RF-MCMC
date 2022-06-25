0456c9a24414671942443c93ef46072faa682ab9
shixinxing <47957281+shixinxing@users.noreply.github.com>
2022-06-26 00:47:54 +0800

    divide initial length-scale by D_in
    having fixed the training problem when training with RBF kernels

--- kernels/RBF.py
+++ kernels/RBF.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 # import tensorflow_probability as tfp
 
 class RBFKernel(tf.Module):
-    def __init__(self, n_feature=1, amplitude=1., length_scale=1., trainable=True, is_ard=False, name=None):
+    def __init__(self, n_feature=1, amplitude=1., length_scale=None, trainable=True, is_ard=False, name=None):
         """
         k(x, y) = amplitude**2 * exp(-||x - y||**2 / (2 * length_scale**2))
         :param n_feature: the last dim of input
@@ -12,6 +12,9 @@ class RBFKernel(tf.Module):
         super(RBFKernel,self).__init__(name=name)
         self.kernel_type = "RBF"
         self.n_feature = n_feature
+        # initialize length scale to \sqrt(d_in)
+        if length_scale is None:
+            length_scale = tf.cast(n_feature, tf.float32) ** 0.5
 
         if tf.rank(length_scale) >= 2: # not a vector
             raise ValueError("The length scale of RBF dim error!")
@@ -29,7 +32,7 @@ class RBFKernel(tf.Module):
 
         self.log_amplitude = tf.Variable(tf.math.log(amplitude), trainable=trainable, name="log_amplitude")
         self.log_inv_length_scale = tf.Variable(tf.math.log(inv_length_scale), trainable=trainable,
-                                                dtype=tf.float32, name="log_length_scale")
+                                                dtype=tf.float32, name="log_inv_length_scale")
 
     @property
     def amplitude(self):
