bd28df46478f6b571a9a79730ffa542f392a41c8
shixinxing <47957281+shixinxing@users.noreply.github.com>
2022-08-24 21:39:37 +0800

    allow assigning W in the layers
    use tf.stop_gradient to stop the gradient from flowing from W while maintaining the GradientTape

--- layers/GP_weight_layers.py
+++ layers/GP_weight_layers.py
@@ -6,7 +6,15 @@ class GPLayer(tf.Module):
         super(GPLayer, self).__init__(name=name)
         self.in_feature = in_feature
         self.out_feature = out_feature
-        self.W = tf.Variable(tf.random.normal([in_feature, out_feature]), name='GP_layer_W')
+        self.W = tf.Variable(tf.random.normal([in_feature, out_feature], mean=0., stddev=1.), name='GP_layer_W')
 
-    def __call__(self, X):
-        return tf.matmul(X, self.W)
+    def __call__(self, X, allow_gradient_from_W=True):
+        if allow_gradient_from_W:
+            return tf.matmul(X, self.W)
+        else: # when W is from assigned value, regard it as constant to prevent gradient tape breaking
+            return tf.matmul(X, tf.stop_gradient(self.W))
+
+    def assign_W(self, W_value):
+        if isinstance(W_value, tf.Variable):
+            W_value = W_value.numpy()
+        self.W.assign(W_value)
